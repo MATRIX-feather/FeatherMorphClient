0d3bc5ff241a59cfc20adca0e9ff72fc26cda6a9
MATRIX-feather <midnightcarnival@outlook.com>
2022-12-13 03:03:33 +0800

    fix: 修复创造模式背包不会正确显示客户端预览的问题
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

---
+++ src/main/java/xiamo/morph/client/mixin/CreativeInventoryMixin.java
@@ -0,0 +1,21 @@
+package xiamo.morph.client.mixin;
+
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import xiamo.morph.client.graphics.InventoryRenderHelper;
+
+@Mixin(CreativeInventoryScreen.class)
+public class CreativeInventoryMixin
+{
+    private static final InventoryRenderHelper helper = InventoryRenderHelper.getInstance();
+
+    @Redirect(method = "drawBackground",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/InventoryScreen;drawEntity(IIIFFLnet/minecraft/entity/LivingEntity;)V"))
+    public void onBackgroundDrawCall(int x, int y, int size, float mouseX, float mouseY, LivingEntity no)
+    {
+        helper.onRenderCall(x, y, size, mouseX, mouseY);
+    }
+}
