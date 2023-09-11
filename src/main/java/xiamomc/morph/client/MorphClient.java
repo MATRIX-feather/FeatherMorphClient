68f95857e17d4a11b0341b157e3969b1ec658917
MATRIX-feather <midnightcarnival@outlook.com>
2023-09-11 19:46:00 +0800

    misc: oops

--- src/main/java/xiamomc/morph/client/MorphClient.java
+++ src/main/java/xiamomc/morph/client/MorphClient.java
@@ -92,7 +92,7 @@ public class MorphClient extends AbstractSchedulablePlugin implements ClientModI
     public ServerHandler serverHandler;
     private ClientSkillHandler skillHandler;
 
-    private final boolean debugToasts = true;
+    private final boolean debugToasts = false;
 
     @Override
     public void onInitializeClient()
