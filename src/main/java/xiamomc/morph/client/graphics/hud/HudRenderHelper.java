07fa3bf4736a6fc7bc2a064e2320deadc3aff0f3
MATRIX-feather <midnightcarnival@outlook.com>
2023-09-12 17:35:10 +0800

    enhance: 优化暴露值显示的视觉效果

--- src/main/java/xiamomc/morph/client/graphics/hud/HudRenderHelper.java
+++ src/main/java/xiamomc/morph/client/graphics/hud/HudRenderHelper.java
@@ -1,5 +1,6 @@
 package xiamomc.morph.client.graphics.hud;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.shedaniel.math.Color;
 import net.minecraft.client.gui.DrawContext;
 import xiamomc.morph.client.ClientMorphManager;
@@ -13,18 +14,22 @@ import xiamomc.pluginbase.Annotations.Initializer;
 import xiamomc.pluginbase.Annotations.Resolved;
 import xiamomc.pluginbase.Bindables.Bindable;
 
+import java.util.Random;
+
 public class HudRenderHelper extends MorphClientObject
 {
     @Resolved
     private ClientMorphManager manager;
 
-    private final Bindable<Float> preferredWidth = new Bindable<>(0f);
-    private final Recorder<Float> widthRecorder = new Recorder<>(0f);
+    private final Bindable<Float> revValueNext = new Bindable<>(0f);
+    private final Recorder<Float> revDisplayRecorder = new Recorder<>(0f);
     private final Recorder<Float> progressHeightRecorder = new Recorder<>(0f);
     private final Recorder<Float> barHeightRecorder = new Recorder<>(0f);
 
     private final Recorder<Color> colorRecord = new Recorder<>(Color.ofOpaque(0));
-    private final Bindable<Color> preferredColor = new Bindable<>(Color.ofOpaque(0));
+    private final Bindable<Color> colorNext = new Bindable<>(Color.ofOpaque(0));
+
+    private final Recorder<Float> drawAlpha = new Recorder<>(0f);
 
     private final Bindable<Color> preferredBgColor = new Bindable<>(Color.ofOpaque(0));
     private final Recorder<Color> bgColorRecord = new Recorder<>(Color.ofOpaque(0));
@@ -34,29 +39,60 @@ public class HudRenderHelper extends MorphClientObject
     @Initializer
     private void load()
     {
-        preferredWidth.bindTo(manager.revealingValue);
-        preferredColor.onValueChanged((o, n) ->
-        {
-            if (!o.equals(n))
-                Transformer.transform(colorRecord, n, 1500, Easing.OutQuint);
-        });
+        revValueNext.bindTo(manager.revealingValue);
 
-        preferredWidth.onValueChanged((o, n) ->
+        colorNext.onValueChanged((o, n) ->
         {
-            Transformer.transform(widthRecorder, n, 2000, Easing.OutQuint);
-        });
+            if (o == null) o = MaterialColors.Indigo500;
+
+            if (!n.equals(o))
+                Transformer.transform(colorRecord, n, visible.get() ? 1500 : 0, Easing.OutQuint);
+        }, true);
+
+        revValueNext.onValueChanged((o, n) ->
+        {
+            Transformer.transform(revDisplayRecorder, n, visible.get() ? 2000 : 0, Easing.OutQuint);
+        }, true);
 
         preferredBgColor.onValueChanged((o, n) ->
         {
-            if (!o.equals(n))
-                Transformer.transform(bgColorRecord, n, 1500, Easing.OutQuint);
-        });
+            if (o == null) o = MaterialColors.Indigo500;
+
+            if (!n.equals(o))
+                Transformer.transform(bgColorRecord, n, visible.get() ? 1500 : 0, Easing.OutQuint);
+        }, true);
 
         visible.onValueChanged((o, n) ->
         {
-            Transformer.transform(progressHeightRecorder, n ? -5f : 0f, 650, Easing.InOutQuint);
-            Transformer.transform(barHeightRecorder, n ? -12f : 0f, 650, Easing.OutBack);
-        });
+            // Bar进度
+            Transformer.transform(progressHeightRecorder, n ? 0f : -2.5f, 650, Easing.InOutQuint);
+
+            // Bar位置
+            Transformer.transform(barHeightRecorder, n ? 0f : -6f, 650, Easing.OutBack);
+
+            Transformer.transform(drawAlpha, n ? 1 : 0f, 650, Easing.OutQuint);
+        }, true);
+
+        this.addSchedule(this::update);
+    }
+
+    private void update()
+    {
+        this.addSchedule(this::update);
+
+        //if (plugin.getCurrentTick() % 40 == 0)
+        //    revValueNext.set(revValueNext.get() > 0f ? 0f : new Random().nextFloat(10, 100));
+
+        var rev = revDisplayRecorder.get();
+
+        var targetClr = (rev >= 20)
+                ? (rev >= 80 ? MaterialColors.Red600 : MaterialColors.Orange500)
+                : MaterialColors.Green400;
+
+        colorNext.set(targetClr);
+        preferredBgColor.set(targetClr.darker(3f));
+
+        visible.set(rev > 0.1f);
     }
 
     public void onRender(DrawContext context, float tickDelta)
@@ -69,20 +105,7 @@ public class HudRenderHelper extends MorphClientObject
         {
             matrices.push();
 
-            var rev = widthRecorder.get();
-            visible.set(rev < 0.1f);
-
-            var targetClr = (rev >= 20)
-                    ? (rev >= 80 ? MaterialColors.Red600 : MaterialColors.Amber500)
-                    : MaterialColors.Green400;
-
-            preferredColor.set(targetClr);
-            preferredBgColor.set(targetClr.darker(3f));
-
-            //matrices.push();
             renderBar(context, tickDelta);
-            //matrices.pop();
-            //renderProgress(context, tickDelta);
         }
         finally
         {
@@ -92,26 +115,42 @@ public class HudRenderHelper extends MorphClientObject
 
     public void renderBar(DrawContext context, float tickDelta)
     {
-        var width = 10;
-        var height = 35;
+        // 10 * 0.8
+        var width = 8;
+
+        // 35 * 0.8
+        var height = 32;
         var padding = 1;
 
         var windowHeight = context.getScaledWindowHeight();
         var matrices = context.getMatrices();
 
+        var shaderColor = RenderSystem.getShaderColor();
+        shaderColor = new float[]
+        {
+            shaderColor[0],
+            shaderColor[1],
+            shaderColor[2],
+            shaderColor[3]
+        };
+
+        context.setShaderColor(1, 1, 1, drawAlpha.get());
+
         // 先位移到屏幕外面
-        matrices.translate(barHeightRecorder.get(), windowHeight, 0);
-
         // 然后再位移到屏幕里面
-        matrices.translate(2, -height - 2, 0);
+        matrices.translate(barHeightRecorder.get() + 2, windowHeight - height - 2, 0);
 
-        context.drawBorder(0, 0, width, height, 0xFF000000);
+        context.drawBorder(0, 0, width, height, bgColorRecord.get().darker(1.3).getColor());
 
+        // 填充背景
         context.fill(padding, padding, width - padding, height - padding, bgColorRecord.get().getColor());
 
-        var barStart = padding + Math.round((height - padding * 2) * (widthRecorder.get() / 100));
+        // 填充进度
+        var barStart = padding + Math.round((height - padding * 2) * (revDisplayRecorder.get() / 100));
         var barEnd = height - padding;
         context.fill(padding, barStart, width - padding, barEnd, colorRecord.get().getColor());
+
+        context.setShaderColor(shaderColor[0], shaderColor[1], shaderColor[2], shaderColor[3]);
     }
 
     public void renderProgress(DrawContext context, float tickDelta)
@@ -125,7 +164,7 @@ public class HudRenderHelper extends MorphClientObject
         matrices.translate(0, progressHeightRecorder.get(), 0);
 
         //var width = context.getScaledWindowWidth();
-        var scale = width - Math.round(width * (widthRecorder.get() / 100));
+        var scale = width - Math.round(width * (revDisplayRecorder.get() / 100));
         // var scale30 = Math.round(width * 0.2f);
         // var scale80 = Math.round(width * 0.8f);
 
